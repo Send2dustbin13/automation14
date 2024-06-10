68c0466b20b81996fdd8a6ecee3851bd44ef0425
Rsati_09 <138803796+Send2dustbin13@users.noreply.github.com>
2024-06-10 21:00:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ st.title("VIP NUMBERS")
 
 col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
-    st.button('Request number')
+    st.button('REQUEST')
 with col2:
     st.button('REPORT')
 with col3:
-    st.button('DELETE NUMBER')
+    st.button('DELETE NO.')
 with col4:
-    st.button('ADD NUMBER')
+    st.button('ADD NO.')
 with col5:
     st.button('SIGN IN')
 
