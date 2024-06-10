eca40296aa0e3941ae1a5fa4486b19d246a2429f
Rsati_09 <138803796+Send2dustbin13@users.noreply.github.com>
2024-06-10 20:30:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
 st.title("ADD NUMBER")
-
-name = st.text_input("ENTER NUMBER")
-email = st.date_input("ENTER DATE")
-number = st.selectbox(SINGLE, DOUBLE, TRIPLE,placeholder="ENTER CATEGORY", label_visibility="visible")
-checkbox = st.checkbox("Get latest news and updates...")
+number = st.text_input("NUMBER")
+date = st.date_input("DATE")
+number = st.text_input("CATEGORY")
+price = st.number_input("PRICE")
 
 save = st.button("Submit")
-cancel = st.button("Cancel")
 if save:
   st.success("Submitted successfully !!!")
