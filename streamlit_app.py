15db3da7fc710c0f1abf15f1bda404c065a642f8
Rsati_09 <138803796+Send2dustbin13@users.noreply.github.com>
2024-06-10 20:32:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ date = st.date_input("DATE")
 number = st.text_input("CATEGORY")
 price = st.number_input("PRICE")
 
-save = st.button("Submit")
+save = st.button("SUBMIT")
 if save:
   st.success("Submitted successfully !!!")
