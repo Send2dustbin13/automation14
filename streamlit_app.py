f0e88f637a4fde7e55dd6d329f48b4ffac5f610e
Rsati_09 <138803796+Send2dustbin13@users.noreply.github.com>
2024-06-10 20:28:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("ADD NUMBER")
 
 name = st.text_input("ENTER NUMBER")
 email = st.date_input("ENTER DATE")
-number = st.selectbox(SINGLE, DOUBLE, TRIPLE, *, placeholder="ENTER CATEGORY", label_visibility="visible")
+number = st.selectbox(SINGLE, DOUBLE, TRIPLE,placeholder="ENTER CATEGORY", label_visibility="visible")
 checkbox = st.checkbox("Get latest news and updates...")
 
 save = st.button("Submit")
