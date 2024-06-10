c3a3afd8627429a1a4aeb13e1ef883d802600e42
Rsati_09 <138803796+Send2dustbin13@users.noreply.github.com>
2024-06-10 20:52:31 +0530

    Create addnumber.py

---
+++ addnumber.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("ADD NUMBER")
+number = st.text_input("NUMBER")
+date = st.date_input("DATE")
+number = st.text_input("CATEGORY")
+price = st.text_input("PRICE")
+
+save = st.button("SUBMIT")
+if save:
+  st.success("SUBMITTED SUCCESSFULLY !!!")
