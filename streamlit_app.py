5f8bf54620047075d0e7e96f297976ba97c2bb9f
Rsati_09 <138803796+Send2dustbin13@users.noreply.github.com>
2024-06-10 20:31:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("ADD NUMBER")
 number = st.text_input("NUMBER")
-date = st.date_input("DATE", format = DD/MM/YYYY)
+date = st.date_input("DATE")
 number = st.text_input("CATEGORY")
 price = st.number_input("PRICE")
 
