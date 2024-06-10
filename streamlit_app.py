1a5b516ba632a504e5146ca76637424243c3d989
Rsati_09 <138803796+Send2dustbin13@users.noreply.github.com>
2024-06-10 15:42:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,12 @@ st.title("SIGN UP")
 
 name = st.text_input("Enter Name")
 email = st.text_input("Enter Email address")
-number = st.text_input("Enter Mobile number")
+number = st.number_input("Enter Mobile number")
 checkbox = st.checkbox("Get latest news and updates...")
 
 save = st.button("Submit")
+cancel = st.button("Cancel")
 if save:
   st.success("Submitted successfully !!!")
+else:
+  st.error("Canceled successfully")
